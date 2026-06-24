d8f5bd2989b59a1de116ef601444d26cf609bda1
Aarib Faraz Ahmed <aaribfaraz2002@gmail.com>
2026-06-24 22:45:32 +0200

    Deploy bug fixes: vendor profile crash and empty cart resolved

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "04d2807380ebaabc0faddc98b4f5a268",
-"/": "04d2807380ebaabc0faddc98b4f5a268",
-"main.dart.js": "0bb1c21c9b66dcab05be6f18749575e7",
+"index.html": "182e531727a03213915bb04aa1607587",
+"/": "182e531727a03213915bb04aa1607587",
+"main.dart.js": "9f2824b5647dfe9cffb5c81d30a7cfea",
 "manifest.json": "0899e62e0aa14aa012b0de41828ecde0",
 "version.json": "fa2cdb82b5de2002c3f030aa50713457"};
 // The application shell files that are downloaded before a service worker can
